ac46895fa7480c7607f59b943043e2cba0554d4d
meyialagan2000-sys <meyialagan2000@gmail.com>
2025-09-05 17:38:50 +0530

    mid point transaction

--- Expence Tracker/Models/Transaction.cs
+++ Expence Tracker/Models/Transaction.cs
@@ -10,7 +10,7 @@ namespace Expence_Tracker.Models
 
        
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
 
 
         public int Amount { get; set; }
@@ -19,6 +19,23 @@ namespace Expence_Tracker.Models
         public string? Note { get; set; }
 
         public DateTime Date { get; set; } = DateTime.Now;
+
+        [NotMapped]
+        public string? CategoryTitleWithIcon
+        {
+            get
+            {
+                return Category == null ? "" : Category.Icon + " " + Category.Title;
+            }
+        }
+        [NotMapped]
+        public string? FormattedAmount
+        {
+            get
+            {
+                return ((Category == null || Category.Type == "Expense") ? "- " : "+ ") + Amount.ToString("C0");
+            }
+        }
 
     }
 }
